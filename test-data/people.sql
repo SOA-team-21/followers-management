dfd1df09efaba52bab2a17907f72ba1dce6a3c9c
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2024-04-25 19:03:03 +0200

    feat: Added delete statements in the beginning of the scripts

--- test-data/people.sql
+++ test-data/people.sql
@@ -1,3 +1,6 @@
+MATCH (n)
+DETACH DELETE n;
+
 CREATE (:People {Id: -12, UserId: -12, Name: 'Lena', Surname: 'Lenić', Picture: '', Bio: '', Quote: '', Email: 'autor2@gmail.com'});
 CREATE (:People {Id: -13, UserId: -13, Name: 'Sara', Surname: 'Sarić', Picture: '', Bio: '', Quote: '', Email: 'autor3@gmail.com'});
 CREATE (:People {Id: -22, UserId: -22, Name: 'Mika', Surname: 'Mikić', Picture: '', Bio: '', Quote: '', Email: 'turista2@gmail.com'});
--- test-data/users.sql
+++ test-data/users.sql
@@ -1,3 +1,5 @@
+DELETE FROM stakeholders."Users";
+
 INSERT INTO stakeholders."Users" ("Id", "Username", "Password", "Role", "Email", "IsActive", "Followers", "Notifications", "IsProfileActivated", "IsBlogEnabled") VALUES (-12, 'autor2@gmail.com', '$2a$11$0kdekuQ8zM28Un.SpFawYuXQEkH0qSp8C1t27TUswmFn1/lnv.Bku', 1, 'autor2@gmail.com', true, '[]', '[]', true, NULL);
 INSERT INTO stakeholders."Users" ("Id", "Username", "Password", "Role", "Email", "IsActive", "Followers", "Notifications", "IsProfileActivated", "IsBlogEnabled") VALUES (-13, 'autor3@gmail.com', '$2a$11$.wlqffgBj.WQG9PirwlR/uyyNRnCo217D0qlSm7Hp/hs52wuf56X2', 1, 'autor3@gmail.com', true, '[]', '[]', true, NULL);
 INSERT INTO stakeholders."Users" ("Id", "Username", "Password", "Role", "Email", "IsActive", "Followers", "Notifications", "IsProfileActivated", "IsBlogEnabled") VALUES (-21, 'turista1@gmail.com', '$2a$11$q71j24egWePnIOjLle1Ol.nnP3tQWbK8683iyGUNkf8DsAJHRAEhO', 2, 'turista1@gmail.com', true, '[]', '[]', true, NULL);
