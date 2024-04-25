cfe86b6dad0fc31ef1d1e71315fdfab707d3e050
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2024-04-25 18:50:20 +0200

    feat: Changed sql insert statements for people.sql

--- test-data/people.sql
+++ test-data/people.sql
@@ -1,5 +1,5 @@
-INSERT INTO stakeholders."People" ("Id", "UserId", "Name", "Surname", "Picture", "Bio", "Quote", "Xp", "Level") VALUES (-12, -12, 'Lena', 'Lenić', '', '', '', 0, 1);
-INSERT INTO stakeholders."People" ("Id", "UserId", "Name", "Surname", "Picture", "Bio", "Quote", "Xp", "Level") VALUES (-13, -13, 'Sara', 'Sarić', '', '', '', 0, 1);
-INSERT INTO stakeholders."People" ("Id", "UserId", "Name", "Surname", "Picture", "Bio", "Quote", "Xp", "Level") VALUES (-22, -22, 'Mika', 'Mikić', '', '', '', 75, 2);
-INSERT INTO stakeholders."People" ("Id", "UserId", "Name", "Surname", "Picture", "Bio", "Quote", "Xp", "Level") VALUES (-23, -23, 'Steva', 'Stević', '', '', '', 75, 2);
-INSERT INTO stakeholders."People" ("Id", "UserId", "Name", "Surname", "Picture", "Bio", "Quote", "Xp", "Level") VALUES (-21, -21, 'Pera', 'Perić', '', '', '', 112, 3);
+CREATE (:People {Id: -12, UserId: -12, Name: 'Lena', Surname: 'Lenić', Picture: '', Bio: '', Quote: '', Email: 'autor2@gmail.com'});
+CREATE (:People {Id: -13, UserId: -13, Name: 'Sara', Surname: 'Sarić', Picture: '', Bio: '', Quote: '', Email: 'autor3@gmail.com'});
+CREATE (:People {Id: -22, UserId: -22, Name: 'Mika', Surname: 'Mikić', Picture: '', Bio: '', Quote: '', Email: 'turista2@gmail.com'});
+CREATE (:People {Id: -23, UserId: -23, Name: 'Steva', Surname: 'Stević', Picture: '', Bio: '', Quote: '', Email: 'turista3@gmail.com'});
+CREATE (:People {Id: -21, UserId: -21, Name: 'Pera', Surname: 'Perić', Picture: '', Bio: '', Quote: '', Email: 'turista1@gmail.com'});
